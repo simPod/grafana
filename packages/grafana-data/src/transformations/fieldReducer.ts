d7003e40aeed2d8ee60cc11d7e100c6d452d4194
Simon Podlipsky <simon@podlipsky.net>
2018-01-16 01:25:47 +0100

    Add 95Pctl reducer

--- packages/grafana-data/src/transformations/fieldReducer.ts
+++ packages/grafana-data/src/transformations/fieldReducer.ts
@@ -18,6 +18,7 @@ export enum ReducerID {
   range = 'range',
   diff = 'diff',
   diffperc = 'diffperc',
+  pctl95 = 'pctl95',
   delta = 'delta',
   step = 'step',
   firstNotNull = 'firstNotNull',
@@ -248,6 +249,12 @@ export const fieldReducers = new Registry<FieldReducerInfo>(() => [
     standard: true,
   },
   {
+    id: ReducerID.pctl95,
+    name: 'P95',
+    description: 'The score at or below which 95 % of the scores in the distribution may be found',
+    standard: false,
+  },
+  {
     id: ReducerID.allValues,
     name: 'All values',
     description: 'Returns an array with all values',
@@ -285,6 +292,7 @@ export function doStandardCalcs(field: Field, ignoreNulls: boolean, nullAsZero: 
     delta: 0,
     step: Number.MAX_VALUE,
     diffperc: 0,
+    pctl95: null,
 
     // Just used for calculations -- not exposed as a stat
     previousDeltaUp: true,
@@ -367,6 +375,13 @@ export function doStandardCalcs(field: Field, ignoreNulls: boolean, nullAsZero: 
 
       calcs.lastNotNull = currentValue;
     }
+  }
+
+  if (isNumberField) {
+    calcs.pctl95 = percentile(
+      [...data.toArray()].sort((a, b) => a - b),
+      0.95
+    );
   }
 
   if (calcs.max === -Number.MAX_VALUE) {
@@ -503,4 +518,15 @@ function calculateDistinctCount(field: Field, ignoreNulls: boolean, nullAsZero: 
     distinct.add(currentValue);
   }
   return { distinctCount: distinct.size };
+}
+
+function percentile(arr: number[], percent: number) {
+  if (arr.length === 0) {
+    return null;
+  }
+
+  const k = (arr.length - 1) * percent;
+  const f = Math.floor(k);
+
+  return arr[f];
 }
