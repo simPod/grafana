40945b48e1a2e1cced7fd3c4598af54cea0c49e0
Simon Podlipsky <simon@podlipsky.net>
2018-03-20 23:09:49 +0100

    Change colors

--- packages/grafana-data/src/themes/palette.ts
+++ packages/grafana-data/src/themes/palette.ts
@@ -15,6 +15,7 @@ export const palette = {
   darkBorder1: '#34343B',
   darkBorder2: '#64646B',
 
+  flop: '#18181b',
   // Dashboard bg / layer 0 (light theme)
   gray90: '#F4F5F5',
   // Card bg / layer 1
